8d96a51d6911149d36f25d8fe307361af9351be9
Haliava <grenka388@gmail.com>
2020-05-12 15:01:51 +0400

    added comments

--- data/users.py
+++ data/users.py
@@ -9,6 +9,15 @@ from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase, UserMixin):
+    """Таблица users в базе данных bad_ui.sqlite
+    
+    Поля:
+    id (integer): id пользователя
+    name (string): ник пользователя (пока он не отображается в комментариях)
+    is_moderator (boolean): является пользователь модератором или нет
+    request_id (integer): id запроса пользователя в модераторы
+    hashed_password (string): хэшированный пароль польщователя, указанный при регистрации
+    """
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -22,13 +31,28 @@ class User(SqlAlchemyBase, UserMixin):
         return f'<User> {self.id} {self.name} {self.email} {self.about}'
 
     def set_password(self, password):
+        """Хэшировать пароль"""
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
+        """Совпадает ли введённый пароль с хэшированным"""
         return check_password_hash(self.hashed_password, password)
 
 
 class Article(SqlAlchemyBase):
+    """Таблица articles в базе данных bad_ui.sqlite
+    
+    Поля:
+    id (integer): id записи
+    thumbnail (string): url заставки новости (отображается на главной странице)
+    is_anonymous (boolean): показывать ник автора или нет
+    author_name (string): ник автора
+    html_code (string): html код записи
+    css_code (string): css код записи
+    script_code (string): js код записи
+    views (integer): просмотры записи (обновляется каждый раз при открытии записи,
+    даже если пользователь уже видел эту запись за текущую сессию)
+    """
     __tablename__ = 'articles'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     thumbnail = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -43,6 +67,15 @@ class Article(SqlAlchemyBase):
 
 
 class Request(SqlAlchemyBase):
+    """Таблица requests в базе данных bad_ui.sqlite
+
+    Поля:
+    id (integer): id запроса
+    introduction (string): представление пользователя
+    about (string): пользователь рассказывает о себе
+    applicant_id (integer): id пользователя, который делает запрос
+    is_request_accepted (boolean): принят запрос или отклонён
+    """
     __tablename__ = 'requests'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     introduction = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -52,24 +85,16 @@ class Request(SqlAlchemyBase):
 
 
 class Comment(SqlAlchemyBase):
+    """Таблица comments в базе данных bad_ui.sqlite
+
+    Поля:
+    id (integer): id комментария
+    article (integer): id комментируемой записи
+    user_id (integer): id комментирующего
+    text (integer): текст комментария
+    """
     __tablename__ = 'comments'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     article = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('articles.id'))
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
     text = sqlalchemy.Column(sqlalchemy.String, nullable=False, default='')
-
-
-# session = create_session()
-# user = session.query(User).filter(User.id == 1).first()
-# news = News(title="Личная запись", content="Эта запись личная",
-#             is_private=True)
-# user.news.append(news)
-# session.commit()
-# user = session.query(User).filter((User.id == 1)).first()
-# print(user)
-# user.name = "Измененное имя пользователя"
-# user.created_date = datetime.datetime.now()
-# session.commit()
-# user = session.query(User).filter(User.id == 3).first()
-# session.delete(user)
-# session.commit()
